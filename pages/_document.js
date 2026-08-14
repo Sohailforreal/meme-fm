cfee80576a99a1909eced8ed3328f36aa5c7b72c
Sohail Shaikh <133530464+Sohailforreal@users.noreply.github.com>
2026-08-15 00:43:47 +0530

    Add preconnect links for YouTube and YouTube images

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://www.youtube.com" />
+        <link rel="preconnect" href="https://i.ytimg.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
        <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif&family=DM+Sans:wght@400;500;600;700&display=swap" rel="stylesheet"/> 
     </Head>
